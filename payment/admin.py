fe190b72eaeb7cb4f63a512b48c5c332003aae61
Armin Azhdehnia <arminmk18@gmail.com>
2020-11-10 05:16:15 -0500

    add relative payment model to admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (ModelAdmin,
+                                                modeladmin_register)
 
-# Register your models here.
+# import model
+from .models import Payment
+
+
+class PaymentModelAdmin(ModelAdmin):
+    """
+        this class is for import Payment class and provide an access from CRM
+    """
+    model = Payment
+    menu_label = 'Payment'
+    menu_icon = 'radio-empty'
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('donation',
+                    'amount',
+                    'status',
+                    'created',
+                    'updated')
+
+
+modeladmin_register(PaymentModelAdmin)  # register PaymentModelAdmin to CRM
