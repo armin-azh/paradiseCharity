c85f1d1a3361094aada0346811536d2be535d8b7
Armin Azhdehnia <arminmk18@gmail.com>
2020-11-10 05:05:56 -0500

    add relative order model to admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (ModelAdmin,
+                                                modeladmin_register)
 
-# Register your models here.
+# import model
+from .models import Order
+
+
+class OrderModelAdmin(ModelAdmin):
+    """
+        this class is for import Order class and provide an access from CRM
+    """
+    model = Order
+    menu_label = 'Order'
+    menu_icon = 'radio-empty'
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('donation', 'order_name', 'status', 'created')
+
+
+modeladmin_register(OrderModelAdmin)  # register OrderModelAdmin to CRM
