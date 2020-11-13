242524f7339cdb1e9179ef4f2428dbb17815962b
Armin Azhdehnia <arminmk18@gmail.com>
2020-11-13 09:43:01 -0500

    change wagtail admin url

--- paraserver/urls.py
+++ paraserver/urls.py
@@ -11,7 +11,7 @@ from search import views as search_views
 urlpatterns = [
     path('django-admin/', admin.site.urls),
 
-    path('admin/', include(wagtailadmin_urls)),
+    path('', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
@@ -27,13 +27,4 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
 
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
