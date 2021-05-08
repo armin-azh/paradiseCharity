5b1210e336c225bbffe96aca9101bff8992a28a4
Armin Azhdehnia <arminmk18@gamil.com>
2021-05-08 17:03:12 -0400

    changed functions and trigers

--- sql_code/functions.sql
+++ sql_code/functions.sql
@@ -1,3 +1,4 @@
+-- Auth_User
 CREATE OR REPLACE FUNCTION check_max_register_user()
 	RETURNS TRIGGER
 	LANGUAGE PLPGSQL
@@ -7,11 +8,43 @@ DECLARE
 	num_users INT;
 BEGIN
 	SELECT COUNT(*) INTO num_users FROM auth_user;
-	IF num_users>5 THEN
+	IF num_users>=5 THEN
 		RAISE 'YOU REACH THE MAXIMUM USERS: %',num_users;
 		RETURN NULL;
 	ELSE
 		RETURN NEW;
 	END IF;
 END;
+$$;
+
+CREATE OR REPLACE FUNCTION update_user_log_in_time()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS
 $$
+BEGIN
+	IF NEW.last_login<>OLD.last_login THEN
+		NEW.last_login = current_timestamp;
+	END IF;
+	RETURN NEW;
+END;
+$$;
+
+
+-- Payment
+CREATE OR REPLACE FUNCTION insert_new_payment()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS
+$$
+DECLARE
+	order_id INT;
+BEGIN
+	SELECT id INTO order_id FROM "order" WHERE donation_id =NEW.donation_id;
+	IF NOT order_id THEN
+		RETURN NEW;
+	ELSE
+		RAISE 'YOU WANT TO CREATE A PAYMENT WITHOUT ANY ORDER SUBMITION';
+	END IF;
+END;
+$$;
--- sql_code/trigers.sql
+++ sql_code/trigers.sql
@@ -1,6 +1,19 @@
 DROP TRIGGER IF EXISTS max_user_policy ON auth_user;
+DROP TRIGGER IF EXISTS update_users_login ON auth_user;
 
 CREATE TRIGGER max_user_policy
 	BEFORE INSERT
 	ON auth_user
 	EXECUTE	PROCEDURE check_max_register_user();
+	
+CREATE TRIGGER update_users_login
+	BEFORE UPDATE
+	ON auth_user
+	EXECUTE PROCEDURE update_user_log_in_time();
+	
+-- PAYMENT
+CREATE TRIGGER insert_payment_with_order_submission
+	BEFORE INSERT
+	ON payment
+	EXECUTE PROCEDURE insert_new_payment();
+	
